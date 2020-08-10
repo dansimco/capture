a81cc95a13cca535c6eb5b1330a3ff953d0b244c
dansimco <ds@dansim.co>
2020-08-10 09:47:33 +0700

    add threshold trigger idea'

--- lib/app.lua
+++ lib/app.lua
@@ -1,9 +1,29 @@
-local app = {}
 local parameters = include("lib/parameters")
 local ui = include("lib/ui")
 
+local poll_freq = 1/15
+
+local app = {
+  input_level_l=0,
+  input_level_r=0,
+}
+
+
 function app.init()
   parameters.init(app)
+
+
+  local level_poll_l = poll.set("amp_in_l")
+  local level_poll_r = poll.set("amp_in_r")
+  level_poll_l.time = poll_freq
+  level_poll_r.time = poll_freq
+  
+  level_poll_l.callback = function(val) app.input_level_l = val end
+  level_poll_r.callback = function(val) app.input_level_r = val end
+
+  level_poll_l:start()
+  level_poll_r:start()
+
 end
 
 function app.redraw()
--- lib/ui.lua
+++ lib/ui.lua
@@ -1,11 +1,75 @@
 local ui = {}
 
+screen.font_face(1)
+screen.font_size(8)
+local threshold = 0.1
+local is_recording = false
+
+
 function ui.redraw(app)
   screen.aa(0)
   screen.clear()
-  screen.move(0, 5)
-  screen.fill()
+  screen.level(16)
+  screen.move(0, 10)
+  screen.text("LEFT: "..app.input_level_l)
+  screen.move(0, 20)
+  screen.text("RIGHT: "..app.input_level_l)
+  screen.move(0, 30)
+  if (is_recording) then 
+    state = "recording"
+  else
+    state = "waiting"
+  end
+  screen.text(state)
+  ui.draw_meters(app.input_level_l, app.input_level_r)
   screen.update()
 end
+
+
+local averaging_frames = 8
+
+ui.level_history_l = {}
+ui.level_history_r = {}
+ui.level_i = 0
+
+function ui.draw_meters(level_l, level_r)
+  screen.level(4)
+
+  ui.level_i = ui.level_i + 1
+  ui.level_history_l[ui.level_i] = level_l
+  ui.level_history_r[ui.level_i] = level_r
+  if (ui.level_i == (averaging_frames+1)) then ui.level_i = 0 end
+  local l=table.reduce(ui.level_history_l, function(a, b) return a+b end) / averaging_frames
+  local r=table.reduce(ui.level_history_r, function(a, b) return a+b end) / averaging_frames
+  ui.average_l = l
+  ui.average_r = r
+
+  local h = 40
+
+  if (level_l > threshold or level_r > threshold) then
+    screen.level(16)
+    is_recording = true
+  elseif (l < threshold and r < threshold) then
+    is_recording = false
+  end
+  screen.rect(119, 1+(64-h*l), 2, h*l)
+  screen.rect(124, 1+(64-h*r), 2, h*r)
+  screen.fill()
+
+end
+
+
+table.reduce = function (list, fn) 
+    local acc
+    for k, v in ipairs(list) do
+        if 1 == k then
+            acc = v
+        else
+            acc = fn(acc, v)
+        end 
+    end 
+    return acc 
+end
+
 
 return ui
